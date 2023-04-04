f1435235f718b056f2f56bb4f103d5468bbdc923
IsaiahXD-8462 <116024098+IsaiahXD-8462@users.noreply.github.com>
2023-04-04 17:09:49 -0400

    imported local setting to settings.py file

--- heroes_villains_project/settings.py
+++ heroes_villains_project/settings.py
@@ -118,3 +118,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from heroes_villains_project.local_settings import *
+except ImportError:
+    raise ImportError("local setting not found")
