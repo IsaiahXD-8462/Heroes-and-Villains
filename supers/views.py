c027ea695b6d0ff19b05b97f38ceff48d10008b3
IsaiahXD-8462 <116024098+IsaiahXD-8462@users.noreply.github.com>
2023-04-06 14:34:55 -0400

    minor changes

--- supers/views.py
+++ supers/views.py
@@ -3,14 +3,13 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import SuperSerializer
-from .models import Product
-from super_types.models import SuperType
+from .models import Super
 
 # Create your views here.
-@api_view(['GET']):
+@api_view(['GET', 'POST'])
 def supers_list(request):
 
-    if request.method == 'GET:'
+    if request.method == 'GET':
         super_types_param = request.query_params.get('super_types')
         sort_param = request.query_params.get('sort')
 
@@ -18,20 +17,20 @@ def supers_list(request):
         serializer = SuperSerializer(supers, many=True)
 
         if super_types_param:
-        supers = supers.filter(super_types__name=super_types_param)
+            supers = supers.filter(super_types__name=super_types_param)
 
         if sort_param:
-        super = supers.order_by(sort_param)
+            super = supers.order_by(sort_param)
 
         return Response('super_types_param')
-        return(sort_param)
+        
     elif request.method == 'POST':
-        serializer = ProductSerializer(data=request.data)
+        serializer = SuperSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-@api_view(['GET']):
+@api_view(['GET'])
 def supers_and_super_types(request):
 
     supers = Super.objects.all()
@@ -42,15 +41,15 @@ def supers_and_super_types(request):
 
     custom_response_dict = {
         'supers': super_serializer.data
-        'super_types': dealership_serializer.data
+        'super_types': super_type_serializer.data
     }
-    return Response(custom_reponse_dict)
+    return Response(custom_response_dict)
 
 @api_view(['GET', 'PUT', 'DELETE'])   
 def super_detail(request, pk):
-    product = get_object_or_404(Product, pk=pk)
+    super = get_object_or_404(Super, pk=pk)
     if request.method =='GET':
-        serializer = SuperSerializer(product);
+        serializer = SuperSerializer(super);
         return Response(serializer.data)
     elif request.method == 'PUT':
         serializer = SuperSerializer(super, data=request.data)
