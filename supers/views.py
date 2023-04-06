99f48de9be4916a246284d73e159a6b267c280d1
IsaiahXD-8462 <116024098+IsaiahXD-8462@users.noreply.github.com>
2023-04-06 10:20:01 -0400

    Added more to Get Request

--- supers/views.py
+++ supers/views.py
@@ -5,11 +5,21 @@ from rest_framework.response import Response
 @api_view(['GET']):
 def supers_list(request):
 
-    super_types_params = request.query_params.get('super_types')
+    super_types_param = request.query_params.get('super_types')
     sort_param = request.query_params.get('sort')
+
+    supers = Super.objects.all()
+
+    if super_types_param:
+        supers = supers.filter(super_types__name=super_types_param)
+
+    if sort_param:
+        super = supers.order_by(sort_param)
+
     return Response('super_types_param')
     return(sort_param)
 
+@api_view(['GET']):
 def supers_and_super_types(request):
 
     supers = Super.objects.all()
